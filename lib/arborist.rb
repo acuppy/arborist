b7e115f6dc26b03e61125df00c4d80d9297a0a8b
Adam Cuppy <adam@codingzeal.com>
2016-07-04 16:59:39 -0700

    Due to load order issues, moved `Arborist.config` to lib/arborist/configuration.rb

--- lib/arborist.rb
+++ lib/arborist.rb
@@ -3,16 +3,4 @@ require 'arborist/version'
 require 'arborist/configuration'
 
 module Arborist
-  class << self
-    def config ns = nil
-      @config     ||= Configuration.new
-      @config[ns] ||= Configuration.new if ns
-
-      if block_given?
-        yield ns ? @config[ns] : @config
-      end
-
-      @config
-    end
-  end
 end
--- lib/arborist/configuration.rb
+++ lib/arborist/configuration.rb
@@ -1,6 +1,17 @@
 require 'ostruct'
 
 module Arborist
+  def self.config ns = nil
+    @config     ||= Configuration.new
+    @config[ns] ||= Configuration.new if ns
+
+    if block_given?
+      yield ns ? @config[ns] : @config
+    end
+
+    @config
+  end
+
   class Configuration < OpenStruct
     def initialize props={}
       super
