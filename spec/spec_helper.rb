099cbc55644325786c5e6a8d0fd6b5eab5d44ad9
Adam Cuppy <adam@codingzeal.com>
2016-07-04 16:49:21 -0700

    Configure RSpec
    - Use Sqlite as the database
    - Teardown the database after each run
    - `define_schema` helper to build a database

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,22 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'pry-byebug'
 require 'arborist'
+
+RSpec.configure do |c|
+  c.after(:all) { teardown_db }
+end
+
+ActiveRecord::Base.establish_connection(
+  adapter: "sqlite3",
+  database: ":memory:"
+)
+
+def define_schema(verbose = false, &schema)
+  ActiveRecord::Schema.verbose = verbose
+  ActiveRecord::Schema.define version: 1, &schema
+end
+
+def teardown_db
+  conn = ActiveRecord::Base.connection
+  conn.tables.each { |t| conn.drop_table t }
+end
