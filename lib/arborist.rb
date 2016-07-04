1acf352dedf7d3c38f28d1adc3c10309af1562e0
Adam Cuppy <adam@codingzeal.com>
2016-07-04 16:35:06 -0700

    Add `Arborist.config` support...
    - Namespaces: `config.migration` will build a new `Configuration` collection
    - Block: `config.migration { |c| c.foo = :bar }`

--- lib/arborist.rb
+++ lib/arborist.rb
@@ -1,5 +1,17 @@
 require "arborist/version"
+require 'arborist/configuration'
 
 module Arborist
-  # Your code goes here...
+  class << self
+    def config ns = nil
+      @config     ||= Configuration.new
+      @config[ns] ||= Configuration.new if ns
+
+      if block_given?
+        yield ns ? @config[ns] : @config
+      end
+
+      @config
+    end
+  end
 end
--- spec/arborist_spec.rb
+++ spec/arborist_spec.rb
@@ -5,7 +5,30 @@ describe Arborist do
     expect(Arborist::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '.config' do
+    context 'without defining a namespace' do
+      it 'creates a blank container at the root level' do
+        expect(Arborist.config).to be_a Arborist::Configuration
+      end
+    end
+
+    context 'when defining a namespaces' do
+      it 'creates an empty container' do
+        Arborist.module_eval { config :test }
+        expect(Arborist.config.test).to be_a Arborist::Configuration
+      end
+    end
+
+    context 'when passing a block' do
+      it 'yields a configurable object' do
+        Arborist.module_eval do
+          config { |c| c.foo = :bar } # on the root
+          config(:test) { |c| c.foo2 = :bar2 } # namespaced
+        end
+
+        expect(Arborist.config.foo).to eq :bar
+        expect(Arborist.config.test.foo2).to eq :bar2
+      end
+    end
   end
 end
